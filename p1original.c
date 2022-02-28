ea6bcd02be923c4cd4e74a59da980e4726a572d1
Kushal <95837058+Kush-007@users.noreply.github.com>
2022-02-28 09:20:10 +0000

    p2original

--- p1original.c
+++ p1original.c
@@ -1,25 +1,31 @@
-#include <stdio.h>
-#include<math.h>
-void input(float *x1,float *y1,float *x2,float *y2)
+#include<stdio.h> 
+void input_line(float*x1,float*y1,float*x2,float*y2,float*x3,float*y3)
 {
-  printf("enter the values of x1,y1,x2,y2: ");
-  scanf("%f%f%f%f",x1,y1,x2,y2);
-
+  printf("enter the value of x1,y1,x2,y2,x3,y3\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
 }
-void find_distance(float x1,float y1,float x2,float y2,float *area)
+int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
 {
-  area=sqrt((x2-x1)(x2-x1)+(y2-y1)*(y2-y1));
+float area;
+  area=0.5*((x1*(y2-y3))(x2(y3-y1))(x3(y1-y2)));
+    return area;
 }
-void output(float x1,float y1,float x2,float y2,float area)
+void output(float x1,float y1,float x2,float y2,float x3,float y3,float area)
 {
-  printf("the distance between %f,%f and %f,%f will be %f",x1,y1,x2,y2,area);
-}
-int main()
+  if (area<=0)
+  {
+    printf("the points %f,%f and %f,%f and %f,%f dosen't form a triangle\n",x1,y1,x2,y2,x3,y3);
+    }
+    else
+  {
+    printf("the points %f,%f and %f,%f and %f,%f form a triangle\n",x1,y1,x2,y2,x3,y3);
+    }
+  }
+  int main()
 {
-  float a,b,c,d,e;
-  input(&a,&b,&c,&d);
-  find_distance(a,b,c,d,&e);
-  output(a,b,c,d,e);
-  return a;
-
-}
+  float a,b,c,d,e,f,result;
+  input_line(&a,&b,&c,&d,&e,&f);
+  result=is_triangle(a,b,c,d,e,f);
+  output(a,b,c,d,e,f,result);
+  return 0;
+  }
